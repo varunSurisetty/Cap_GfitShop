1805ca2c3d3b289e999196bd3294bcedd752e17e
varun <varun@wipro.com>
2021-05-31 00:32:35 +0530

    commit on 31/05 11:10 AM

--- src/main/java/devops/cap/controllerWithoutDb/UserLoginController.java
+++ src/main/java/devops/cap/controllerWithoutDb/UserLoginController.java
@@ -1,11 +1,12 @@
 package devops.cap.controllerWithoutDb;
 
-import java.io.IOException;
+//import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
--- src/main/java/devops/cap/controllerWithoutDb/UserPasswordController.java
+++ src/main/java/devops/cap/controllerWithoutDb/UserPasswordController.java
@@ -1,10 +1,11 @@
 package devops.cap.controllerWithoutDb;
 
-import java.io.IOException;
+//import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
